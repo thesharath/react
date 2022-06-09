05e7c2662fcb2740221324e2e68cfe92a835940c
Sharath Madineni <sharath.madineni@hexagon.com>
2022-06-09 18:29:05 +0530

    Updates

---
+++ precache-manifest.13632a460ce428d3137c34c023c2d677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d18ee8139123b2bd01e8",
+    "url": "/robofriends/static/css/main.7adb3bcb.chunk.css"
+  },
+  {
+    "revision": "d18ee8139123b2bd01e8",
+    "url": "/robofriends/static/js/main.d18ee813.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/robofriends/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "5d9f402a201a3c4bdc03",
+    "url": "/robofriends/static/css/2.f37edce5.chunk.css"
+  },
+  {
+    "revision": "5d9f402a201a3c4bdc03",
+    "url": "/robofriends/static/js/2.5d9f402a.chunk.js"
+  },
+  {
+    "revision": "db08b936970c55357179",
+    "url": "/robofriends/static/js/runtime~main.db08b936.js"
+  },
+  {
+    "revision": "7eab59032fa0751a166464e688aa5379",
+    "url": "/robofriends/index.html"
+  }
+];
